1b11445de6e54383528f4b864de47d8152dffd4c
Kai T. Ohlhus <k.ohlhus@gmail.com>
2021-05-24 18:33:58 +0900

    JupyterLab: sync with upstream

--- jupyterlab/jupyter_notebook_config.py
+++ jupyterlab/jupyter_notebook_config.py
@@ -12,8 +12,6 @@ c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 
-c.OctaveKernel.inline_toolkit = 'qt'
-
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
 
